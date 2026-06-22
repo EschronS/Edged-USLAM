756e2550b05e0e82a11cc0d17ee9dabf6893538b
erik schlenzka <erik.schlenzka@gmx.de>
2026-06-22 17:40:43 +0200

    run script with eport params

--- rpg_ultimate_slam_open/applications/ze_vio_ceres/launch/run_uslam.sh
+++ rpg_ultimate_slam_open/applications/ze_vio_ceres/launch/run_uslam.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 sr
+ROSNODE_PATH="/home/edged_uslam/src/Edged-USLAM/rpg_ultimate_slam_open/applications/ze_vio_ceres"
 BAGPATH="${1:-$(find /home/bags -name '*.bag' | fzf)}"
 BAGNAME=$(basename "$BAGPATH" .bag)
-LOGPATH="/tmp/${BAGNAME}_$(date '+%F_%H-%M-%S')/"
-mkdir -p /tmp/edged_uslam_run
+LOGPATH="/tmp/${BAGNAME}_edged_$(date '+%F_%H-%M-%S')/"
+mkdir -p /tmp/uslam_run
 roslaunch ze_vio_ceres dvx_micro_events_only.launch bag_filename:="${BAGPATH}"
 mkdir -p "$LOGPATH"
-cp /tmp/edged_uslam_run/* "$LOGPATH"
+cp /tmp/uslamrun/* "$LOGPATH"/
+# copy config and launch files to results folder
+cp "$ROSNODE_PATH"/launch/dvx_micro_events_only.launch "$ROSNODE_PATH"/cfg/vio_base.conf "$ROSNODE_PATH"/cfg/vio_dvx.conf "$LOGPATH"/
+# copy traj and param files to were the bag lives
+cp -r "$LOGPATH" $(dirname "$BAGPATH")/
